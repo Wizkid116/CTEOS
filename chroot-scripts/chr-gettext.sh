bc7a90c002f044db9a2a7756caf41095c53c41b5
Kansnex <114298273+Wizkid116@users.noreply.github.com>
2024-04-21 16:54:57 -0500

    files to run in chroot(still a lot to create though)

---
+++ chroot-scripts/chr-gettext.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+. $DIST_ROOT/build_env/build_scripts/inc-start.sh $1 $(basename $0) 
+
+./configure --disable-shared
+
+make
+
+cp -v gettext-tools/src/{msgfmt,msgmerge,xgettext} /usr/bin
+
+. $DIST_ROOT/build_env/build_scripts/inc-end.sh $1 $(basename $0) 
